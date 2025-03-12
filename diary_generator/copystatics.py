4098cc284ed25cbe607165b8eca519c40fa2814c
gorou12 <webs@pokete.com>
2025-03-12 22:08:27 +0900

    展開パス修正

--- diary_generator/copystatics.py
+++ diary_generator/copystatics.py
@@ -2,5 +2,5 @@ import shutil
 
 
 def copy_static_files():
-    shutil.copytree("static", "output/src", dirs_exist_ok=True)
+    shutil.copytree("static", "output", dirs_exist_ok=True)
     print("✅静的ファイルコピー完了！")
