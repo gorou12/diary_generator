3758df1a6f3b35f085f370c9e05b847966c0751c
gorou12 <webs@pokete.com>
2025-10-22 23:18:47 +0900

    サムネイルにlargeサイズを追加したのをpictureタグに追加

--- diary_generator/util/img/image.py
+++ diary_generator/util/img/image.py
@@ -8,12 +8,13 @@ def generate_image_tag(id: str, url: str, alt_text="") -> str:
     # サムネイルパスを取得
     small_thumbnail = get_thumbnail_path(id, "small")
     medium_thumbnail = get_thumbnail_path(id, "medium")
-
+    large_thumbnail = get_thumbnail_path(id, "large")
     # pictureタグでレスポンシブ画像を生成
     return f'''<a href="{local_path}" target="_blank">
   <picture>
     <source media="(max-width: 400px)" srcset="{small_thumbnail}" type="image/webp">
     <source media="(min-width: 401px)" srcset="{medium_thumbnail}" type="image/webp">
-    <img class="embedimg" src="{medium_thumbnail}" alt="{alt_text}" loading="lazy">
+    <source media="(min-width: 721px)" srcset="{large_thumbnail}" type="image/webp">
+    <img class="embedimg" src="{large_thumbnail}" alt="{alt_text}" loading="lazy">
   </picture>
 </a>'''
